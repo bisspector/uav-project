1e562ac952bda668ae853eca518cbfc7be1ad0f0
bisspector <bisspector@gmail.com>
2019-11-10 02:13:54 +0200

    Clean up ignored files

--- app/routes.py
+++ app/routes.py
@@ -20,8 +20,10 @@ main = Construct()
 drone = 0
 location = 0
 
+
 def get_dist(a, b):
     return sqrt((a["x"] - b["x"]) * (a["x"] - b["x"]) + (a["y"] - b["y"]) * (a["y"] - b["y"]))
+
 
 def get_good_angle(a, b):
     A = {"x": b["x"] - a["x"], "y": b["y"] - a["y"]}
@@ -41,13 +43,15 @@ def get_good_angle(a, b):
 def is_good(x, y):
     return x > l and y > l and x + l < SIZE and y + l < SIZE
 
+
 def get_good(x, y):
     x = x * 4032 // 800 + 984
     y = y * 3024 // 600 + 1488
     return x, y
-     
+
+
 def solve_dataset(old_im, obj):
-    
+
     UPLOAD_FOLDER_DATASET = './tmp/kek/'
     ZIP_DATASET = './tmp/photoset.zip'
     IMAGE_URL_DATASET = './tmp/lol.jpg'
@@ -62,7 +66,7 @@ def solve_dataset(old_im, obj):
     lx = 2 * h * tan(angle_x / 360 * 3.1415926)
     ly = 2 * h * tan(angle_y / 360 * 3.1415926)
     res = []
-    base = {"lat": 50.015781, "lng": 36.222751} #Ivriy's house
+    base = {"lat": 50.015781, "lng": 36.222751}  # Ivriy's house
     phi = base["lat"] / 180 * 3.1415926
     dpx = 111.321*cos(phi)
     dpy = 111.143
@@ -85,11 +89,11 @@ def solve_dataset(old_im, obj):
     old_size = old_im.size
 
     new_size = (6000, 6000)
-    new_im = Image.new("RGB", new_size)   
+    new_im = Image.new("RGB", new_size)
     new_im.paste(old_im, ((new_size[0]-old_size[0])//2,
-                      (new_size[1]-old_size[1])//2))
+                          (new_size[1]-old_size[1])//2))
 
-    #new_im.save(IMAGE_URL_DATASET)
+    # new_im.save(IMAGE_URL_DATASET)
     cur = 0
     for point in points:
         point["x"], point["y"] = get_good(point["x"], point["y"])
@@ -104,43 +108,45 @@ def solve_dataset(old_im, obj):
             cur["x"] = points[cur_i+1]["x"]
             cur["y"] = points[cur_i+1]["y"]
             angle = get_good_angle(points[cur_i], points[cur_i + 1])
-            
-            rotate_angle = (90 + int(angle / 3.1415926 * 180)  + 360) % 360
+
+            rotate_angle = (90 + int(angle / 3.1415926 * 180) + 360) % 360
             phi = -rotate_angle / 180 * 3.1415926
             cur["x"] -= 3000
             cur["y"] -= 3000
-            
+
             x = int(cur["x"] * cos(phi) - cur["y"] * sin(phi))
             y = int(cur["x"] * sin(phi) + cur["y"] * cos(phi))
             x += 3000
             y += 3000
             cur["x"] += 3000
             cur["y"] += 3000
-            
+
             #img = Image.open(IMAGE_URL_DATASET)
             img = new_im.copy()
             tmp = str(photo_id)
             while len(tmp) < 4:
                 tmp = '0' + tmp
-            img.rotate(rotate_angle).crop((x - lx//2, y - lx//2, x + ly//2, y + ly//2)).save(UPLOAD_FOLDER_DATASET + 'result_' + tmp + '.jpg')
+            img.rotate(rotate_angle).crop((x - lx//2, y - lx//2, x + ly//2,
+                                           y + ly//2)).save(UPLOAD_FOLDER_DATASET + 'result_' + tmp + '.jpg')
             photo_id += 1
-            res.append({"alt_rel": h, "yaw": 90+angle / 3.1415926 * 180, "pitch": 0, "roll": 0, "lng": cur["x"] / 1000 / dpx + base["lng"], "lat": base["lat"] - cur["y"] / 1000 / dpy})
+            res.append({"alt_rel": h, "yaw": 90+angle / 3.1415926 * 180, "pitch": 0, "roll": 0,
+                        "lng": cur["x"] / 1000 / dpx + base["lng"], "lat": base["lat"] - cur["y"] / 1000 / dpy})
             res[-1]["lat"] *= 1e7
             res[-1]["lng"] *= 1e7
             res[-1]["lat"] = int(res[-1]["lat"])
             res[-1]["lng"] = int(res[-1]["lng"])
-            
+
             cur_i = cur_i + 1
             if cur_i + 1 == len(points):
                 break
-            
+
         else:
             angle = get_good_angle(points[cur_i], points[cur_i + 1])
             cur["x"] += nd * cos(angle)
             cur["y"] += nd * sin(angle)
-            rotate_angle = (90 + int(angle / 3.1415926 * 180)  + 360) % 360
+            rotate_angle = (90 + int(angle / 3.1415926 * 180) + 360) % 360
             phi = -rotate_angle / 180 * 3.1415926
-            
+
             cur["x"] -= 3000
             cur["y"] -= 3000
             x = int(cur["x"] * cos(phi) - cur["y"] * sin(phi))
@@ -149,10 +155,11 @@ def solve_dataset(old_im, obj):
             y += 3000
             cur["x"] += 3000
             cur["y"] += 3000
-            
+
             #img = Image.open(IMAGE_URL_DATASET)
             img = new_im.copy()
-            res.append({"alt_rel": h, "yaw": 90+angle / 3.1415926 * 180, "pitch": 0, "roll": 0, "lng": cur["x"] / 1000 / dpx + base["lng"], "lat": base["lat"] - cur["y"] / 1000 / dpy})
+            res.append({"alt_rel": h, "yaw": 90+angle / 3.1415926 * 180, "pitch": 0, "roll": 0,
+                        "lng": cur["x"] / 1000 / dpx + base["lng"], "lat": base["lat"] - cur["y"] / 1000 / dpy})
             res[-1]["lat"] *= 1e7
             res[-1]["lng"] *= 1e7
             res[-1]["lat"] = int(res[-1]["lat"])
@@ -160,46 +167,44 @@ def solve_dataset(old_im, obj):
             tmp = str(photo_id)
             while len(tmp) < 4:
                 tmp = '0' + tmp
-            img.rotate(rotate_angle).crop((x - lx//2, y - lx//2, x + ly//2, y + ly//2)).save(UPLOAD_FOLDER_DATASET + 'result_' + tmp + '.jpg')
+            img.rotate(rotate_angle).crop((x - lx//2, y - lx//2, x + ly//2,
+                                           y + ly//2)).save(UPLOAD_FOLDER_DATASET + 'result_' + tmp + '.jpg')
             photo_id += 1
             nd = lx * (1 - overlapping)
-    #images[0].show()
-    for i in range(0, len(res)):     
+    # images[0].show()
+    for i in range(0, len(res)):
         new_pos = {"meta": {"type": "CAMERA_FEEDBACK"}, "data": res[i].copy()}
         res[i] = new_pos.copy()
     with open(UPLOAD_FOLDER_DATASET + 'logs.json', 'w') as outfile:
-        for i in range(0, len(res)):     
+        for i in range(0, len(res)):
             print(json.dumps(res[i]), file=outfile)
     top = UPLOAD_FOLDER_DATASET
-    
+
     with ZipFile(ZIP_DATASET, 'a') as zipObj:
         for folderName, subfolders, filenames in os.walk(top, topdown=False):
             for filename in filenames:
                 filePath = os.path.join(folderName, filename)
                 zipObj.write(filePath, '/' + filename)
-    
+
     url = '/tmp/photoset.zip?'
-    
+
     url = url + str(randint(0, 1000000))
     return url
-
-       
 
 
 @app.route('/send', methods=['GET', 'POST'])
 def main():
     points = request.get_json()
     old_im = Image.open('./basic.jpg')
-    
+
     return solve_dataset(old_im, points)
-    
+
+
 @app.route('/tmp/<path:filename>')
 def uploaded_file(filename):
     print(filename)
     return send_from_directory('C:/Users/Maxim/Desktop/zahar1/tmp/',
                                filename, as_attachment=True)
-
-
 
 
 @app.route('/sendHomePosition', methods=['POST'])
@@ -243,6 +248,7 @@ def sendInfo():
     return jsonify(drone)
 
 # END DRON COVERAGE
+
 
 @app.route('/')
 def index():
