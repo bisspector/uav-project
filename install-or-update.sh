2f5aae7cd4c2d336118ca1289dba1131a6e28f76
bisspector <bisspector@gmail.com>
2019-11-11 12:01:24 +0200

    added scripts for ubuntu

---
+++ install-or-update.sh
@@ -0,0 +1,10 @@
+
+sudo apt install python3 python3-pip python3-venv redis mongodb
+rm -rf env
+git pull
+python3 -m venv env
+source env/bin/activate
+pip3 install wheel setuptools -r requirements.txt
+rq worker &
+flask run
+deactivate
